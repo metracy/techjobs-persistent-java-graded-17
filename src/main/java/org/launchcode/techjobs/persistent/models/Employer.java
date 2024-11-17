bb93c31f04bcb647c7bd3e38d94ae7486ac3a1c3
MET <matthew.etracy@gmail.com>
2024-11-17 16:44:12 -0600

    Repositories, Controllers, and SQL query added for task2

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,23 @@
 package org.launchcode.techjobs.persistent.models;
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank(message = "Location needed.")
+    @Size(max = 100, message = "Characters up to 100")
+    private String location;
+
+    public Employer() {
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
