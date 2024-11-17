2bd10e461c4fce155938503432b7c6f3f2c4dd40
MET <matthew.etracy@gmail.com>
2024-11-17 15:20:07 -0600

    Mapped superclass

--- src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
@@ -4,15 +4,19 @@ package org.launchcode.techjobs.persistent.models;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.util.Objects;
 
+@MappedSuperclass
 public abstract class AbstractEntity {
-
+    @Id
+    @GeneratedValue
     private int id;
-
+    @Size(max = 50, min = 1, message = "Only 50 characters Length Max")
+    @NotBlank(message = "Name cannot be empty")
     private String name;
 
     public int getId() {
